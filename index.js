a5ae4026bca95c48e980e963a61ffe9e7ef4200b
puneet <puneetvashsiht@gmail.com>
2017-10-30 12:03:40 +0530

    more

---
+++ index.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+const PORT = process.env.OPENSHIFT_NODEJS_PORT || 8000;
+const IP = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+function handleRequest(request, response) {
+  response.end(`Web server running on Node.js v${process.versions.node}.`);
+}
+
+var server = http.createServer(handleRequest);
+
+server.listen(PORT, IP, function() {
+  console.log(`Server listening on http://${IP}:${PORT}`);
+});
