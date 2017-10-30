c34749fa9fbd73a76d19f2d6bc7ba87a5285caad
puneet <puneetvashsiht@gmail.com>
2017-10-30 12:41:27 +0530

    t3

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.OPENSHIFT_NODEJS_PORT || 8000;
 const IP = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 function handleRequest(request, response) {
-  response.end(`Web server 2 changed back running on Node.js v${process.versions.node}.`);
+  response.end(`Web server 3 changed back running on Node.js v${process.versions.node}.`);
 }
 
 var server = http.createServer(handleRequest);
